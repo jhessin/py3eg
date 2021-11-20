0e4fabda020f6f4c3309c7b02166fcadaabff9e4
Jim Hessin <jhessin@gmail.com>
2021-11-20 16:05:57 -0700

    added entry widget

--- tkinter-tutorial/app.py
+++ tkinter-tutorial/app.py
@@ -1,7 +1,9 @@
 """ The App class contains all app logic """
 from tkinter import (
     Label,
+    StringVar,
     Tk,
+    Entry,
 )
 
 
@@ -9,6 +11,8 @@ class App:
     """The App class holds all global app variables"""
     root: Tk
     label: Label
+    entry: Entry
+    entry_text: StringVar
 
     def __init__(self, root) -> None:
         # start by saving the root
@@ -20,15 +24,21 @@ class App:
         )
         self.label.pack()
 
+        self.entry_text = StringVar()
+        self.entry = Entry(root, textvariable=self.entry_text)
+        self.entry.pack()
+
         # end by calling the initializer
         self.initialize_window()
 
     def initialize_window(self):
         """ Initializes the app """
 
+        # Two ways to set widget properties
         # self.label['text'] = "New label text"
         # self.label['font'] = ["Courier", 40]
         self.label.configure(text="New label text", font=['Courier', 30])
+        self.label.configure(textvariable=self.entry_text)
 
         self.root.title("My app")
         # sets the initial size (doesn't work in grid layouts)

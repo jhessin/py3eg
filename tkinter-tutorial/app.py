621a75ffa4a73688850cedd7484cd5b0abc2e8bd
Jim Hessin <jhessin@gmail.com>
2021-11-23 06:06:32 -0700

    Update tkinter-app

--- tkinter-tutorial/app.py
+++ tkinter-tutorial/app.py
@@ -1,12 +1,12 @@
 """ The App class contains all app logic """
 from tkinter import (
-    Label,
-    StringVar,
     Tk,
+    Variable,
+    Listbox,
     Entry,
     Button,
-    Listbox,
-    Variable,
+    Label,
+    font,
 )
 from typing import List
 
@@ -14,8 +14,8 @@ from typing import List
 class App:
     """The App class holds all global app variables"""
     root: Tk
-    label_text: StringVar
-    entry_text: StringVar
+    label_text: Variable
+    entry_text: Variable
     list_item_strings: List[str]
 
     def __init__(self, root) -> None:
@@ -27,8 +27,8 @@ class App:
         # sets the maximum size of the window (Does work in grid layouts)
         # root.maxsize(500, 400)
 
-        self.entry_text = StringVar()
-        self.label_text = StringVar(value='New label text')
+        self.entry_text = Variable()
+        self.label_text = Variable(value='New label text')
 
         # A label is a simple text object
         label = Label(
@@ -50,6 +50,7 @@ class App:
         )
 
         button = Button(root, text="Submit", command=self.press_button)
+        button.configure(width=10, height=2, font=('Courier', 40))
 
         self.list_item_strings = ['Hey', 'Hi', 'Hello', 'Howdy', 'Greetings']
         list_items = Variable(value=self.list_item_strings)
@@ -70,10 +71,12 @@ class App:
         # entry.place(x=50, y=50)
         # label.place(x=50, y=10)
 
-        label.grid(column=1, row=1)
-        entry.grid(column=3, row=1)
-        button.grid(column=1, row=2, sticky='SEW')
-        listbox.grid(column=2, row=2)
+        # label.grid(column=1, row=1)
+        # entry.grid(column=3, row=1)
+        # button.grid(column=1, row=2, sticky='SEW')
+        # listbox.grid(column=2, row=2)
+
+        button.place(x=0, y=0)
 
     def press_button(self):
         """Handle pressing the button"""
